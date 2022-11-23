66e708ef49b5ab50a02940ff15ec7e317fd7d5ef
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2022-11-23 21:28:27 +0800

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import HeroBanner from '../components/HeroBanner';
+
+const Home = () => {
+  return (
+    <Box>
+        <HeroBanner />
+    </Box>
+  )
+}
+
+export default Home
