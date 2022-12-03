45a50b6b9b4604f51e6f389bb25ecafefd1f6e11
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2022-12-03 17:50:06 +0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import Exercises from "../components/Exercises";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([]);
+
   return (
     <>
       <HeroBanner />
-      <SearchExercises />
+      <SearchExercises 
+        setExercises={setExercises} 
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises 
+        setExercises={setExercises} 
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </>
   )
 }
