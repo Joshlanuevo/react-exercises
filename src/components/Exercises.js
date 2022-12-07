bb00364155db1d5ec3f8dbafd1ef8038a902b78d
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2022-12-07 23:39:10 +0800

    Update Exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,8 +1,19 @@
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises);
 
-const Exercises = () => {
   return (
-    <div>Exercises</div>
+    <div id="exercises" className="lg:w-28 mt-12 p-5">
+      <div className="text-4xl whitespace-nowrap mb-12">
+        <h4>Showing Results</h4>
+      </div>
+      <div className="lg:w-28 sm:w-14 flex flex-row flex-wrap justify-center">
+        {exercises.map((exercise, index) => (
+          <p>{exercise.name}</p>
+        ))}
+      </div>
+    </div>
   )
 }
 
